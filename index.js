138d385c2635a4add71214fca42dd08bed5b1f9e
terry <tre371@gmail.com>
2015-07-23 10:28:45 -0700

    changes

--- index.js
+++ index.js
@@ -1,19 +1,153 @@
 var Hapi = require('hapi');
 
+// Create a server with a host and port
 var server = new Hapi.Server();
-server.connection({port: 3000});
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the routes
 
 server.route({
-    method:'GET',
+    method: 'GET',
     path: '/{param*}',
     handler: {
-        directory:{
+        directory: {
             path: 'public',
             listing: true
         }
     }
 });
 
+server.route({
+    method: 'GET',
+    path:'/flickr',
+    handler: function (request, reply) {
+        var credentials = require('./public/js/credentials.js'), // <script src="public/js/credentials.js"></script>
+            httpRequest = require('request'),
+            data = {
+                "method": 'flickr.photos.search',
+                "api_key": credentials.flickr.api_key,
+                "tags": 'vancouver',
+                "format": 'json',
+                "nojsoncallback": 1
+            },
+            options = {
+                "uri": 'https://api.flickr.com/services/rest/',
+                "qs": data
+            };
+
+        httpRequest(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('flickr content coming soon') // CLI
+                reply(body); // Show the HTML for the Google homepage in Browser output
+            }
+        })
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/google',
+    handler: function (request, reply) {
+        var httpRequest = require('request');
+
+        httpRequest('http://www.google.com', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('CLI')
+                reply(body); // Show the HTML for the Google homepage in Browser output
+            }
+        })
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+        reply('hello world');
+    }
+});
+
+// Start the server
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
+
+
+
+
+
+
+//var Hapi = require('hapi');
+//
+//var server = new Hapi.Server();
+//server.connection({
+//    host: 'localhost',
+//    port: 3000
+//});
+//
+//server.route({
+//    method:'GET',
+//    path: '/{param*}',
+//    handler: {
+//        directory:{
+//            path: 'public',
+//            listing: true
+//        }
+//    }
+//});
+//
+//server.route({
+//    method:'GET',
+//    path: '/flickr',
+//    handler: function(request, reply){
+//        var credentials = require('./public/js/credentials.js'),// <script src="public/js/credentials.js"></script>
+//            httpRequest = require('request'),
+//        data = {
+//            "method": 'flickr.photos.search',
+//            "api_key": credentials.flickr.api_key,
+//            "tags": 'vancouver',
+//            "format": 'json',
+//            "nojsoncallback":1
+//
+//        },
+//        options = {
+//            "uri":'https://api.flickr.com/services/rest/',
+//            "qs": data
+//        };
+//
+//        httpRequest(options, function(error, response, body) {
+//            if (!error && response.statusCode == 200) {
+//                console.log('flickr content coming soon') // Show the HTML for the Google homepage.
+//                reply(body);
+//            }
+//        })
+//
+//
+//
+//    }
+//});
+//
+//server.route({
+//    method:'GET',
+//    path: '/google',
+//    handler: function(request, reply){
+//        var httpRequest = require('request');
+//
+//        httpRequest('http://www.google.com', function (error, response, body) {
+//            if (!error && response.statusCode == 200) {
+//                console.log('CLI') // Show the HTML for the Google homepage.
+//                reply(body);
+//            }
+//        })
+//
+//    }
+//});
+//
+//
 //server.route({
 //    method:'GET',
 //    path: '/hello',
@@ -21,11 +155,18 @@ server.route({
 //        reply('Hello, world!');
 //    }
 //});
+//
+//
+//
+//server.start(function(){
+//    console.log('Server running at:', server.info.uri);
+//});
+//
+//
 
 
 
-server.start(function(){
-    console.log('Server running at:', server.info.uri);
-});
+
+
 
 
---
+++ public/js/credentials.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tpaugh on 7/22/2015.
+ */
+
+var credentials = {
+    "flickr":{
+        "api_key": 'a62c63831934b4bd1f605cf9e1f196a5'
+
+    }
+};
+
+if(typeof module !== "undefined"){
+    module.exports = credentials;
+}
