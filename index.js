f7278ff739294081c8b665eb67f6ab030201b8a4
terry <tre371@gmail.com>
2015-07-23 12:53:14 -0700

    more

--- index.js
+++ index.js
@@ -31,6 +31,9 @@ server.route({
                 "api_key": credentials.flickr.api_key,
                 "tags": 'vancouver',
                 "format": 'json',
+                "media":'photos',
+                "extras":'url_m',
+                "content_type":1,
                 "nojsoncallback": 1
             },
             options = {
@@ -74,95 +77,6 @@ server.route({
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
-
-
-
-
-
-
-
-//var Hapi = require('hapi');
-//
-//var server = new Hapi.Server();
-//server.connection({
-//    host: 'localhost',
-//    port: 3000
-//});
-//
-//server.route({
-//    method:'GET',
-//    path: '/{param*}',
-//    handler: {
-//        directory:{
-//            path: 'public',
-//            listing: true
-//        }
-//    }
-//});
-//
-//server.route({
-//    method:'GET',
-//    path: '/flickr',
-//    handler: function(request, reply){
-//        var credentials = require('./public/js/credentials.js'),// <script src="public/js/credentials.js"></script>
-//            httpRequest = require('request'),
-//        data = {
-//            "method": 'flickr.photos.search',
-//            "api_key": credentials.flickr.api_key,
-//            "tags": 'vancouver',
-//            "format": 'json',
-//            "nojsoncallback":1
-//
-//        },
-//        options = {
-//            "uri":'https://api.flickr.com/services/rest/',
-//            "qs": data
-//        };
-//
-//        httpRequest(options, function(error, response, body) {
-//            if (!error && response.statusCode == 200) {
-//                console.log('flickr content coming soon') // Show the HTML for the Google homepage.
-//                reply(body);
-//            }
-//        })
-//
-//
-//
-//    }
-//});
-//
-//server.route({
-//    method:'GET',
-//    path: '/google',
-//    handler: function(request, reply){
-//        var httpRequest = require('request');
-//
-//        httpRequest('http://www.google.com', function (error, response, body) {
-//            if (!error && response.statusCode == 200) {
-//                console.log('CLI') // Show the HTML for the Google homepage.
-//                reply(body);
-//            }
-//        })
-//
-//    }
-//});
-//
-//
-//server.route({
-//    method:'GET',
-//    path: '/hello',
-//    handler: function(request, reply){
-//        reply('Hello, world!');
-//    }
-//});
-//
-//
-//
-//server.start(function(){
-//    console.log('Server running at:', server.info.uri);
-//});
-//
-//
 
 
 
