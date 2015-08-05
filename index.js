1c7693863182443bc127c9d32f30b84866f77ea4
terry <tre371@gmail.com>
2015-08-05 10:57:34 -0700

    i dont fucking know

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 var Hapi = require('hapi');
 
 // Input[] should be Flickr's photo details including server, secret, farm
-// Output[] is the full JPG web address
+// Output[] is an oject with `photo` a full JPG web address, `lat` and `lon`
 var createJpgPath = function (photos) {
     var i,
         photo,
-        photoSrc = [],
+        output = [],
         len = photos.length;
     for (i = 0; i < len; i++) {
         photo = photos[i];
-        // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-        photoSrc.push("https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg");
+        //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+        output.push({
+            "photoSrc": "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg",
+            "lat": photo.latitude,
+            "lon": photo.longitude
+
+        });
+
+
     }
-    return photoSrc;
+    return output;
 };
 
 // Create a server with a host and port
 var server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+server.connection({
+    host: 'localhost',
+    port: 8000
 });
 
 // Add the routes
@@ -37,7 +44,7 @@ server.route({
 
 server.route({
     method: 'GET',
-    path:'/flickr', 
+    path: '/flickr',
     handler: function (request, reply) {
         var credentials = require('./public/js/credentials.js'), // <script src="public/js/credentials.js"></script>
             httpRequest = require('request'),
@@ -52,23 +59,19 @@ server.route({
                 "uri": 'https://api.flickr.com/services/rest/',
                 "qs": data
             };
-
-        if(request.query && request.query.tags){
-            options.qs.tags = request.query.tags;
-        }
-         if(request.query && request.query.lat){
-            options.qs.lat = request.query.lat;
-        }
-        if(request.query && request.query.lon){
-            optios.qs.lon = request.query.lon;
-        }
-        if(request.query && request.query.radius){
-            options.qs.radius = request.query.radius;
+        // todo inclass["tags", "lat", "lon", "radius","has_geo", "extras" ]replace if statement with loop
+        var field,
+            fields = ["tags", "lat", "lon", "radius","has_geo", "extras" ];
+        for(var i=0, len=fields.length; i < len; i++){
+            field = fields[i];
+            if(request.query && request.query[field]){
+                options.qs[field]= request.query[field];
+            }
         }
 
         httpRequest(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                console.log('flickr content coming soon') // CLI
+
                 var output = {
                     "photos": createJpgPath(JSON.parse(body).photos.photo)
                 };
@@ -80,10 +83,10 @@ server.route({
 
 server.route({
     method: 'GET',
-    path:'/google', 
+    path: '/google',
     handler: function (request, reply) {
         var httpRequest = require('request');
-        
+
         httpRequest('http://www.google.com', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log('CLI')
@@ -95,9 +98,9 @@ server.route({
 
 server.route({
     method: 'GET',
-    path:'/hello', 
+    path: '/hello',
     handler: function (request, reply) {
-       reply('hello world');
+        reply('hello world');
     }
 });
 
